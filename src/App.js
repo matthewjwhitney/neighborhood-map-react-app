e106e3e1684d621abfdda110cf77e008b134b427
Matthew J Whitney <matthewjwhitney@gmail.com>
2018-05-19 08:48:49 -0700

    create map component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Map from './components/Map.js'
 class App extends Component {
   render() {
     return (
@@ -10,12 +10,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Map />
       </div>
     );
   }
 }
-
 export default App;
